da4a23b4f3b04210ce77fc8df5437ff9ddb2904e
Anton Ivanov <ivanova21600@mail.ru>
2025-11-02 21:31:32 +0300

    refactoring

--- src/main/java/ru/ivanov/ecommerceplatformproject/userservice/config/KafkaConfig.java
+++ src/main/java/ru/ivanov/ecommerceplatformproject/userservice/config/KafkaConfig.java
@@ -1,7 +1,6 @@
 package ru.ivanov.ecommerceplatformproject.userservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
